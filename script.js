c1d7828da734aa13e4f2dcde97a69d3d4608861e
riynmazi <aurinawan.other@gmail.com>
2025-07-07 11:02:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -98,23 +98,21 @@ if (logoWrapper) {
   });
 }
 
-document.getElementById('logoWrapper').addEventListener('click', () => {
-  const logo = document.getElementById('logoWrapper');
+const logoWrapper = document.querySelector('.logo-wrapper');
 
-  // Gerakan acak ke kiri/kanan/atas/bawah max 20px
-  const randX = Math.floor(Math.random() * 40 - 20); // -20 to +20
-  const randY = Math.floor(Math.random() * 40 - 20);
+logoWrapper.addEventListener('click', () => {
+  const randX = (Math.random() - 0.5) * 100 + 'px';
+  const randY = (Math.random() - 0.5) * 100 + 'px';
 
-  logo.style.setProperty('--jX', `${randX}px`);
-  logo.style.setProperty('--jY`, `${randY}px`);
+  logoWrapper.style.setProperty('--rand-x', randX);
+  logoWrapper.style.setProperty('--rand-y', randY);
 
-  logo.classList.add('jump');
+  logoWrapper.classList.add('jump');
 
   setTimeout(() => {
-    logo.classList.remove('jump');
-  }, 400); // waktu animasi = 0.4s
+    logoWrapper.classList.remove('jump');
+  }, 600);
 });
-
 
 
 // === COPY TO CLIPBOARD (untuk donate.html) ===
